83c1ad41fe294d4f3bf6bcbde9042ade9bbad806
Deyan Kalinov <Deyan Kalinov>
2016-05-23 17:52:33 +0300

    .

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,5 +1,5 @@
 Chef::Log.info("Creating cron job for S3 Sync")
-env = Hash["AWS_ACCESS_KEY_ID" => "#{ node[:deploy]['scripts'][:environment_variables][:AWS_ACCESS_KEY_ID]}", "AWS_SECRET_ACCESS_KEY" => "#{ node[:deploy]['scripts'][:environment_variables][:AWS_SECRET_ACCESS_KEY]}", "REGION" => "#{ node[:deploy]['scripts'][:environment_variables][:region]}"]
+env = Hash["AWS_ACCESS_KEY_ID" => "#{ node[:deploy]['scripts'][:environment_variables][:AWS_ACCESS_KEY_ID]}", "AWS_SECRET_ACCESS_KEY" => "#{ node[:deploy]['scripts'][:environment_variables][:AWS_SECRET_ACCESS_KEY]}", "REGION" => "#{ node[:deploy]['scripts'][:environment_variables][:REGION]}"]
 
 cron 'S3_sync' do
 	user'ec2-user'
