33ed43447d08dfbc88c167666b904a6b1ea8b3a5
Deyan Kalinov <Deyan Kalinov>
2016-05-23 16:42:10 +0300

    .

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,2 +1,15 @@
 
- Chef::Log.info("aws_access_key_id: cd #{ node[:deploy]['scripts'][:environment_variables][:aws_access_key_id]} ")
+ Chef::Log.info("Creating cron job for S3 Sync")
+
+ cron 'name_of_cron_entry' do
+  minute '0'
+  hour '20'
+  day '*'
+  month '11'
+  weekday '1-5'
+  environment {"aws_access_key_id" => "#{ node[:deploy]['scripts'][:environment_variables][:aws_access_key_id]}",
+				"aws_secret_access_key" => "#{ node[:deploy]['scripts'][:environment_variables][:aws_secret_access_key],
+				"region" => "#{ node[:deploy]['scripts'][:environment_variables][:region]}}
+  command "aws s3 sync /mnt/nfs s3://mybucket.deyan "
+  action :create
+end
