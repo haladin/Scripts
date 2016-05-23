a9a58724ef81f1b5d5d0b7e645f29958d0b8b6dd
Deyan Kalinov <Deyan Kalinov>
2016-05-23 17:41:04 +0300

    .

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -4,9 +4,13 @@ env = Hash["AWS_ACCESS_KEY_ID" => "#{ node[:deploy]['scripts'][:environment_vari
 				"AWS_SECRET_ACCESS_KEY" => "#{ node[:deploy]['scripts'][:environment_variables][:AWS_SECRET_ACCESS_KEY]}",
 				"REGION" => "#{ node[:deploy]['scripts'][:environment_variables][:region]}"]
 
-cron 's3_sync' do
-	hour '17'
-	environment env
-	command 'aws s3 sync /mnt/nfs s3://mybucket.deyan'
-	action :create
+cron 'name_of_cron_entry' do
+  minute '0'
+  hour '20'
+  day '*'
+  month '11'
+  weekday '1-5'
+  environment env
+  command "aws s3 sync /mnt/nfs s3://mybucket.deyan "
+  action :create
 end
