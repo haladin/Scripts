6525dfb45a7b7a6ce7fad495799c7ad6fea7dac7
Deyan Kalinov <Deyan Kalinov>
2016-05-23 17:48:49 +0300

    .

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,16 +1,14 @@
 Chef::Log.info("Creating cron job for S3 Sync")
+env = Hash["AWS_ACCESS_KEY_ID" => "#{ node[:deploy]['scripts'][:environment_variables][:AWS_ACCESS_KEY_ID]}", "AWS_SECRET_ACCESS_KEY" => "#{ node[:deploy]['scripts'][:environment_variables][:AWS_SECRET_ACCESS_KEY]}", "REGION" => "#{ node[:deploy]['scripts'][:environment_variables][:region]}"]
 
-env = Hash["AWS_ACCESS_KEY_ID" => "#{ node[:deploy]['scripts'][:environment_variables][:AWS_ACCESS_KEY_ID]}",
-				"AWS_SECRET_ACCESS_KEY" => "#{ node[:deploy]['scripts'][:environment_variables][:AWS_SECRET_ACCESS_KEY]}",
-				"REGION" => "#{ node[:deploy]['scripts'][:environment_variables][:region]}"]
-
-cron 'name_of_cron_entry' do
-  minute '0'
-  hour '20'
-  day '*'
-  month '11'
-  weekday '1-5'
-  environment env
-  command "aws s3 sync /mnt/nfs s3://mybucket.deyan "
-  action :create
+cron 'S3_sync' do
+	user'ec2-user'
+	minute '0'
+	hour '20'
+	day '*'
+	month '11'
+	weekday '1-5'
+	environment env
+	command "aws s3 sync /mnt/nfs s3://mybucket.deyan "
+	action :create
 end
